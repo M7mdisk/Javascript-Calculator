3f2855c034a387723f80da20cbf89307e3bdb682
M7mdisk <M7md.k.isk@gmail.com>
2020-12-09 10:46:33 +0300

    initial commit

---
+++ calculator.js
@@ -0,0 +1,111 @@
+let currDisplay, prevDisplay, pressedOperand, prevOperand;
+createNumButtons()
+
+const operands = Array.from(document.getElementsByClassName("operand"));
+const operators = Array.from(document.getElementsByClassName("operator"));
+const display = document.querySelector("[value='screen']");
+
+operands.forEach(key => key.addEventListener("click", updateScreen))
+operators.forEach(key => key.addEventListener("click", operate))
+
+const numbers ="1234567890"
+document.addEventListener('keydown', function(event){
+    let key = event.key
+    if (numbers.includes(key)){
+        if (display.textContent.toString().length < 13)
+            display.textContent = Number(display.textContent + key);
+    }
+} );
+
+function createNumButtons() {
+    const calc = document.getElementById("calculator")
+    for(let i=0; i<10;i++) {
+        var num = document.createElement('div');
+        num.className = "operand";
+        num.id="calcElement";
+        num.setAttribute("data-type", "num")
+        num.innerText = i;
+        num.setAttribute("value", i)
+        calc.appendChild(num);
+    }
+}
+
+initValues();
+
+function initValues() {
+    currDisplay = "";
+    prevDisplay = "";
+    pressedOperand = false;
+    prevOperand = "";
+    display.textContent = "0";
+}
+
+function updateScreen(e) {
+    let input = e.target.textContent;
+    currScreen = Number(display.textContent);
+    if (pressedOperand == true) {
+        display.textContent = "";
+    }
+    if (input == "AC") {
+        initValues();
+    }
+    else if (input == "DEL") {
+        let currScreen = display.textContent.toString();
+        if (currScreen.length > 0) display.textContent = currScreen.slice(0, -1);
+    }
+    else if (input == "±") display.textContent = -currScreen;
+    else if (input == ".") {
+        if (display.textContent.toString().indexOf(".") < 0 &&
+            display.textContent.toString().length < 13)
+            display.textContent += input;
+    }
+    else {
+        if (display.textContent.toString().length < 13)
+            display.textContent = Number(display.textContent + input);
+    }
+    currDisplay = Number(display.textContent);
+    if (display.textContent.toString().length < 1) display.textContent = 0;
+    if (isNaN(currDisplay)) currDisplay = "";
+    pressedOperand = false;
+}
+
+function operate(e) {
+    const operation = e.target.dataset.val;
+    currDisplay = Number(display.textContent);
+    performOp(prevOperand);
+    handleLongNumbers()
+    display.textContent = currDisplay;
+    prevOperand = operation;
+    prevDisplay = currDisplay;
+    pressedOperand = true;
+}
+
+function handleLongNumbers() {
+    if (currDisplay > 9999999999999) currDisplay = "Too large."
+    else if (currDisplay < -9999999999999) currDisplay = "Too small."
+    if (currDisplay.toString().length > 13) currDisplay = currDisplay.toString().slice(0, 13)
+}
+
+function performOp(op) {
+    switch (op) {
+        case "add":
+            currDisplay += Number(prevDisplay);
+            break;
+        case "subtract":
+            currDisplay = Number(prevDisplay) - currDisplay;
+            break;
+        case "multiply":
+            currDisplay *= Number(prevDisplay);
+            break;
+        case "divide":
+            if (currDisplay == 0) {
+                alert("Never divide by zero!")
+                break;
+            }
+            currDisplay = Number(prevDisplay) / currDisplay;
+            break;
+        case "equals":
+            prevDisplay = Number(display.textContent);
+            break;
+    }
+}
